b4259f34fdc3ea9342b8cf170d831a871d52dfe6
VisualViolet <savannah_b_miller@outlook.com>
2023-01-10 20:50:23 -0800

    added 5 day forecast api request and JS

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,17 @@
 const apiKey = "f170c4477d9ee4c56a98f0324cfdbeb4";
 const unit = "imperial";
-const currentDate = "(" + dayjs().format("M/D/YYYY") + ")";
+var currentDate = (new Date()).toLocaleDateString('en-US');
 
 var searchBtn = document.getElementById("search-btn");
 const cityEl = document.getElementById("city-name");
 const tempEl = document.getElementById("temp");
 const windEl = document.getElementById("wind");
 const humidityEl = document.getElementById("humidity");
+const citySearchEl = document.getElementById("search-city");
 
 
 function getWeather(){
-    var city = document.getElementById("city-search").value;
+    var city = document.getElementById("city-input").value;
     var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + unit + "&appid=" + apiKey;
     fetch(requestURL)
     .then(function(response){
@@ -23,13 +24,57 @@ function getWeather(){
         var humidity = (data.main.humidity + "%");
         var icon = (data.weather[0].icon);
         var iconEl = document.createElement("img");
-        cityEl.innerHTML = (city + " " + currentDate);
+        cityEl.innerHTML = (city + " " + "(" + currentDate + ")");
         iconEl.setAttribute("src", "http://openweathermap.org/img/w/" + icon + ".png");
         cityEl.append(iconEl);
-        tempEl.innerHTML = ("Temperature: " + temperature);
+        tempEl.innerHTML = ("Temperature: " + temperature + "\u00B0");
         windEl.innerHTML = ("Wind: " + wind + " MPH");
         humidityEl.innerHTML = ("Humidity: " + humidity);
 
+        
+
+        requestURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=" + unit + "&appid=" + apiKey;
+        fetch(requestURL)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+            var forecastDays = document.querySelectorAll(".col");
+            for (let i = 0; i < forecastDays.length; i++) {
+                forecastDays[i].innerHTML = "";
+                var forecastDataIndex = i * 8 + 2;
+
+                var forecastDate = new Date();
+                forecastDate.setDate(forecastDate.getDate() + (i+1));
+                forecastDate = forecastDate.toLocaleDateString('en-US');
+
+                var forecastDateEl = document.createElement("h3");
+                forecastDateEl.textContent = forecastDate;
+                forecastDays[i].append(forecastDateEl);
+
+                var icon = data.list[forecastDataIndex].weather[0].icon;
+                var forecastIconEl = document.createElement("img");
+                forecastIconEl.setAttribute("src", "http://openweathermap.org/img/w/" + icon + ".png");
+                forecastDays[i].append(forecastIconEl);
+
+
+                var temperature = data.list[forecastDataIndex].main.temp;
+                var forecastTempEl = document.createElement("p");
+                forecastTempEl.textContent = ("Temperature: " + temperature + "\u00B0");
+                forecastDays[i].append(forecastTempEl);
+
+                var wind = data.list[forecastDataIndex].wind.speed;
+                var forecastWindEl = document.createElement("p");
+                forecastWindEl.textContent = ("Wind: " + wind + " MPH");
+                forecastDays[i].append(forecastWindEl);
+
+                var humidity = data.list[forecastDataIndex].main.humidity;
+                var forecastHumidityEl = document.createElement("p");
+                forecastHumidityEl.textContent = ("Humidity: " + humidity);
+                forecastDays[i].append(forecastHumidityEl);
+
+            }
+        })
     })
 }
 
